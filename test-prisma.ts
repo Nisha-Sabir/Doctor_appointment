5a44bb6c446ff46aaddaf1d81ac3484b931d5f07
Doctor Admin <doctor@clinic.com>
2026-08-28 01:48:57 -0700

    fix: Remove unused prisma/nextauth files, clean dependencies, add --webpack to build

--- test-prisma.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { prisma } from "./lib/prisma";
-
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log("Users:", users);
-}
-main().catch(console.error);
